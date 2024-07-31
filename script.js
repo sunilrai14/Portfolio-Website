542ff3ec9135439bd062866e1415c972b0cb7296
Sunil Rai <mail4sunilr@gmail.com>
2024-07-31 13:42:31 +0530

    Seventh Commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-/*===========================================scroll sections active link ========================================== */ 
+/*===========================================toggle icon navbar ========================================== */
+    let menuIcon = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menuIcon.onclick = () => {
+                menuIcon.classList.toggle('bx-x');
+                navbar.classList.toggle('active');
+    };
+
+
+/*=========================================== scroll sections active link ========================================== */ 
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -21,4 +31,9 @@ window.onscroll = () => {
         /*============================ sticky navbar ==================================== */
         let header = document.querySelector('header');
         header.classList.toggle("sticky", window.scrollY > 100);
+
+        /*============================= remove toggle icon and navbar when click navbar link (scroll) ========================================== */
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+
 };
